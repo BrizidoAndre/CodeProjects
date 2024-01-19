02f53bddf3b716b8fa947fac85bcdeea83835593
abrizidobasilio@gmail.com <abrizidobasilio@gmail.com>
2024-01-18 21:52:53 -0300

    fim do commit da aula

--- VSConnect_Kotlin/app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
+++ VSConnect_Kotlin/app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
@@ -2,8 +2,14 @@ package com.example.vsconnect_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vsconnect_kotlin.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    //É uma propriedade privada como o nome vinfing do tipo Activity Login Binding
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
