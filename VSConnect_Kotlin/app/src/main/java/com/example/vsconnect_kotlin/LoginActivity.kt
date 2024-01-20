28bf249ecf735c4e3b388b58cc81f24be8a36c39
abrizidobasilio@gmail.com <abrizidobasilio@gmail.com>
2024-01-19 21:54:54 -0300

    ultimo commit

--- VSConnect_Kotlin/app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
+++ VSConnect_Kotlin/app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vsconnect_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vsconnect_kotlin.databinding.ActivityLoginBinding
@@ -13,5 +14,21 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //atribui a variável binding um objeto que contém referências (propriedades) aos elementos definidos no layout
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        binding.btnEntrar.setOnClickListener{
+            //variável mainIntent com a intenção de sair da Login Activity e ir para MainActivity
+            val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+
+            //executa a intenção armazenada na variável mainIntent
+            startActivity(mainIntent)
+
+            //finaliza a LoginActivity
+            finish()
+        }
+
+        setContentView(binding.root)
     }
 }
